d2784fa41aae1ee25720671755da51b31b0a8adf
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2022-08-14 22:59:32 +0100

    role guard added and integrated with user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, SetMetadata, UseGuards } from '@nestjs/common';
+import { RolesGuard } from 'src/guards/roles.guard';
 
 @Controller('users')
-export class UsersController {}
+@UseGuards(RolesGuard)
+export class UsersController {
+  @SetMetadata('roles', ['admin'])
+  create(@Body() body: any) {
+    return 'create response body';
+  }
+}
