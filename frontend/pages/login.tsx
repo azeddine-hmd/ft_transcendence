0919c649c79434bdc65ccceb99eb82dbfea93e85
Mohammed amine Ajdahim <majdahim@e1r5p9.1337.ma>
2022-08-22 11:31:46 +0100

    adding login page with fancy button on center

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Loginbtn from "../components/auth/Loginbtn";
+
+export default function Login() {
+    return (
+        <>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <Loginbtn/>
+
+        </>
+    );
+}
