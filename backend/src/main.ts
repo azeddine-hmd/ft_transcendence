1da8e745e79da2b9c955af9e019f5ec9815d831d
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2022-08-27 16:06:00 +0100

    swagger added for api documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CrossOriginInterceptor } from './cross-origin.interceptor';
 
@@ -8,6 +9,15 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new CrossOriginInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Ping Pong online Game')
+    .setDescription('Ping Pong online backend service api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 
