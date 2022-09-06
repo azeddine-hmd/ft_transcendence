1f1564d216f0d13c9786c2cd269df916dcecf2dd
Taha Khabouss <tkhabous@e1r4p6.1337.ma>
2022-09-06 22:27:21 +0100

    adding search feature

--- frontend/components/chat/ListView.tsx
+++ frontend/components/chat/ListView.tsx
@@ -1,5 +1,5 @@
 import Card from "./Card";
-import React, { useState, useEffect, FormEventHandler } from 'react';
+import React, { useEffect, useState } from 'react';
 import rooms from '../../rooms.json'
 import style from '../../styles/chat/ListView.module.css'
 import { io } from "socket.io-client";
@@ -7,42 +7,27 @@ import stylee from '../../styles/chat/Card.module.css'
 
 var socket = io('http://localhost:8080', { transports: ['websocket'] });
 
-
-
 function CreateNewRoom() {
-    
+
     const [password, setPassword] = useState('');
-    const handlePasswordChange = (event:React.KeyboardEvent<HTMLInputElement>) => {
-        setPassword(event.currentTarget.value);
-        
-    };
-    
     const [description, setDescription] = useState('');
-    const handleDescriptionChange = (event:React.KeyboardEvent<HTMLInputElement>) => {
-        setDescription(event.currentTarget.value);
-        
-    };
-    
     const [title, setTitle] = useState('');
-    const handleTitleChange = (event:React.KeyboardEvent<HTMLInputElement>) => {
-        setTitle(event.currentTarget.value);
-        
-    };
     
-    const handleSubmitCreateNewRoom = () => {   
+    const handlePasswordChange = (event:React.KeyboardEvent<HTMLInputElement>) => { setPassword(event.currentTarget.value); };
+    const handleDescriptionChange = (event:React.KeyboardEvent<HTMLInputElement>) => { setDescription(event.currentTarget.value); };
+    const handleTitleChange = (event:React.KeyboardEvent<HTMLInputElement>) => { setTitle(event.currentTarget.value); };
+    
+    const handleSubmitCreateNewRoom = () => {
+
         if (title === '' || description === '')
-        {
-            alert('all fields marked (*) must ne filled');
-            return;
-        }
-    
+            return alert('all fields marked (*) must ne filled');
         socket.emit('createRoom', { "title": title, "description": description, "privacy": true, "password": password, "owner": { "id": 1, "name": null } });
     }
-    
+
     return (
         <div className={stylee.row}>
             <div className={stylee.column}>
-                <div className={stylee.card}>
+                <div className={stylee.noHoverCard}>
                     <h3>Create New Room</h3>
                     <input placeholder="title *" className={stylee.input} type="text" onInput={handleTitleChange}></input><br />
                     <input placeholder="description *" className={stylee.input} type="text" onInput={handleDescriptionChange}></input><br />
@@ -55,44 +40,78 @@ function CreateNewRoom() {
             </div>
         </div>
     );
+
 }
 
-export default function ListView() {
-    const [data, setData] = useState(rooms);
+function OnRoomsClick() { socket.emit('findAllRooms'); }
+function OnDMsClick() { socket.emit('findFriends'); }
 
-    socket.emit('findAllRooms')
+var pageLoaded = false;
+
+export default function ListView() {
+
+    const [data, setData] = useState(rooms);
+    const [tmp, setTMP] = useState(rooms);
+    const [channel, setChannel] = useState('rooms');
+
+    if (!pageLoaded){
+        socket.emit('findAllRooms');
+        pageLoaded = true;
+    }
+
     socket.on('createRoom', ({ created }) => {
-        console.log(created);
         if (created)
             socket.emit('findAllRooms');
         else
             alert('something went wrong :(');
-    })
+    });
+
+    socket.on('findFriends', ({ friends }) => {
+        setData(friends)
+        setTMP(friends);
+        setChannel('friends');
+    });
+
     socket.on('findAllRooms', ({ rooms }) => {
-        setData(rooms)
-    })
+        setData(rooms);
+        setTMP(rooms);
+        setChannel('rooms');
+    });
+
+    const onSearch = (event:React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.currentTarget.value.trim() != '')
+        {
+            const result = tmp.filter(function(data){
+                return data.title.includes(event.currentTarget.value);
+            });
+            setData(result);
+        }
+        else {
+            socket.emit('findAllRooms');
+        }
+    };
 
     return (
         <div className={style.list}>
             <div className={style.buttonsHolder}>
-                <button className={style.button}>Rooms</button>
-                <button className={style.button}>DM</button>
+                <button className={style.button} onClick={OnRoomsClick} >Rooms</button>
+                <button className={style.button} onClick={OnDMsClick} >DMs</button>
             </div>
             <div className={style.searchHolder}>
-                <input className={style.searchBar} type="text" placeholder="Search.."></input>
+                <input className={style.searchBar} type="text" placeholder="Search.." onInput={onSearch}></input>
             </div>
             <div className={style.scroll}>
             <div>
-                <CreateNewRoom/>
+                { channel === 'rooms' ? <CreateNewRoom/> : null }
             </div>
-                {data.map(room => {
+                {data.map(data => {
                     return (
-                        <Card title={room.title} description={room.description} members={room.members} id={room.uri} />
+                        <Card title={data.title} description={data.description} members={data.members} id={data.id} />
                     );
                 })}
             </div>
 
         </div>
-
     );
+
 }
