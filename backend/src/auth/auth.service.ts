4fb4715778f0df014147c54932d2dbca303617ea
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2022-09-21 06:33:46 +0100

    revoking old token; documenting apis; storing token in db; etc

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,11 @@
 import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { User } from '../users/entities/user.entity';
 import { UsersService } from '../users/users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { LoginPayloadDto } from './dto/login-payload.dto';
+import { CreateUserDto } from './dto/payload/create-user.dto';
+import { LoginResponseDto } from './dto/response/login-response.dto';
 import { UserPayloadDto } from './dto/user-payload.dto';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,8 +17,8 @@ export class AuthService {
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
 
+    if (!user || !user.password) return null;
     if (user && (await bcrypt.compare(pass, user.password))) {
-      //TODO: encrypt password before storing it in database
       Logger.log(`AuthService#validateUser: validation was success!`);
       const { password, ...result } = user;
       return result;
@@ -28,29 +28,32 @@ export class AuthService {
   }
 
   async registerUser(createUserDto: CreateUserDto): Promise<User> {
-    const userFound = await this.usersService.findOne(createUserDto.username);
-    if (userFound) {
-      Logger.error(`AuthService#registerUser: failed! user exist!`);
-      throw new ForbiddenException();
-    }
-
     // hashing password
     createUserDto.password = await bcrypt.hash(
       createUserDto.password,
       await bcrypt.genSalt(),
     );
 
-    const user = this.usersService.create(createUserDto);
+    const user = await this.usersService.create(createUserDto);
+
+    if (!user) {
+      Logger.error(`AuthService#registerUser: failed! user exist!`);
+      throw new ForbiddenException();
+    }
+
     Logger.log(
       `AuthService#registerUser: user '${user.username}' register is successful!`,
     );
+
     return user;
   }
 
-  async login(user: UserPayloadDto): Promise<LoginPayloadDto> {
+  async login(user: UserPayloadDto): Promise<LoginResponseDto> {
     Logger.log(`AuthService#login: user '${user.username}' logged-in!`);
+    const access_token = this.jwtService.sign(user);
+    await this.usersService.updateUser(user.userId, { token: access_token });
     return {
-      access_token: this.jwtService.sign(user),
+      access_token: access_token,
     };
   }
 }
--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,19 +1,45 @@
+import { Logger, UnauthorizedException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get('JWT_SECRET'),
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+  async validate(request: Request, payload: any) {
+    Logger.debug(`JwtStrategy#validate: starting...`);
+    // get incoming token
+    const auth = request.headers.authorization;
+    if (!auth || auth.split(' ').length !== 2)
+      throw new UnauthorizedException();
+    const incomingToken = auth.split(' ')[1];
+
+    // get stored token and verify expiration time
+    const user = await this.usersService.findOne(payload.userId);
+    if (!user || !user.token) throw new UnauthorizedException();
+    //verify token or not ?
+
+    // compare incoming token with stored token
+    // throw error if not equal
+    if (user.token && user.token !== incomingToken) {
+      Logger.error('valid token but have been revoked with a new one!');
+      throw new UnauthorizedException();
+    }
+
+    return { username: payload.username, userId: payload.userId };
   }
 }
