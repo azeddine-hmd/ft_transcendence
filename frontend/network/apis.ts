a2ca0d9e8912a720aba7da923f4c213021f7948b
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2022-10-03 04:08:23 +0100

    fix more apis

--- frontend/network/apis.ts
+++ frontend/network/apis.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { AddFriendDto } from "./dto/payload/add-friend.dto";
+import { ProfilesUser } from "./dto/payload/profileuser";
 import { SigninDto } from "./dto/payload/signin.dto";
 import { SignupDto } from "./dto/payload/signup.dto";
 import { ErrorResponse } from "./dto/response/error-response.dto";
@@ -8,10 +9,11 @@ import { ProfileResponse } from "./dto/response/profile-response.dto";
 import { RelationResponse } from "./dto/response/relation-response.dto";
 import { SigninResponse } from "./dto/response/signin-response.dto";
 import { localService } from "./local.service";
-import { ProfilesUser } from "./dto/payload/profileuser";
-import { RiCoinsLine } from "react-icons/ri";
 
 export namespace Apis {
+  /*
+    login user
+  */
   export async function Signin(options: {
     signDto: SigninDto;
     onSuccess: (data: SigninResponse) => void;
@@ -32,6 +34,9 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    register user
+  */
   export async function SignUp(options: {
     signupDto: SignupDto;
     onSuccess: () => void;
@@ -52,19 +57,16 @@ export namespace Apis {
   }
 
   
+  /*
+    get other user profile
+  */
   export async function ProfilesUser(options:{
     username: ProfilesUser;
     onSuccess: (profile: ProfileResponse) => void;
     onFailure: (err: ErrorResponse) => void;
   }) {
     try{
-      console.log(options.username);
-      
-      const res = await localService.get(`api/profiles/username`, {
-        params: {
-          username: options.username.username,
-        }
-      });
+      const res = await localService.get<ProfileResponse>(`api/profiles/username/${options.username.username}`);
       return options.onSuccess(res.data);
     }catch (err: any) {
       if (axios.isAxiosError(err)) {
@@ -78,6 +80,9 @@ export namespace Apis {
     
   }
 
+  /*
+    get current user profile
+  */
   export async function CurrentProfile(options: {
     onSuccess: (profile: ProfileResponse) => void;
     onFailure: (err: ErrorResponse) => void;
@@ -96,6 +101,9 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    authenticate with 42 intra
+  */
   export async function autherizeFortytwo() {
     const authenticationUrl = process.env.NEXT_PUBLIC_API_BASE_URL + "/api/auth/intra";
     window.location.assign(authenticationUrl);
@@ -119,6 +127,9 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    verify current user credentials
+  */
   export async function Verify(options: {
     onSuccess: () => void;
     onFailure: (err: ErrorResponse) => void;
@@ -137,13 +148,16 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    send friend request or accept friend request
+  */
   export async function AddFriend(options: {
     addFriendDto: AddFriendDto,
     onSuccess: () => void;
     onFailure: (err: ErrorResponse) => void;
   }) {
     try {
-      await localService.get("/api/users/relations/friend", { data: options.addFriendDto });
+      await localService.post("/api/users/relations/friend", options.addFriendDto);
       return options.onSuccess();
     } catch (err: any) {
       if (axios.isAxiosError(err)) {
@@ -156,13 +170,16 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    remove friend
+  */
   export async function RemoveFriend(options: {
     addFriendDto: AddFriendDto,
     onSuccess: () => void;
     onFailure: (err: ErrorResponse) => void;
   }) {
     try {
-      await localService.get("/api/users/relations/unfriend", { data: options.addFriendDto });
+      await localService.post("/api/users/relations/unfriend", options.addFriendDto);
       return options.onSuccess();
     } catch (err: any) {
       if (axios.isAxiosError(err)) {
@@ -175,6 +192,9 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    returns friends list with relations
+  */
   export async function getFriends(options: {
     onSuccess: (friends: FriendsResponse[]) => void;
     onFailure: (err: ErrorResponse) => void;
@@ -193,17 +213,16 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    returns relation between current user and other user
+  */
   export async function getRelation(options: {
     username: string;
     onSuccess: (relation: RelationResponse) => void;
     onFailure: (err: ErrorResponse) => void;
   }) {
     try {
-      const res = await localService.get<RelationResponse>(`/api/users/relations/with`, {
-        params: {
-          username: options.username,
-        },
-      });
+      const res = await localService.get<RelationResponse>(`/api/users/relations/username/${options.username}`);
       return options.onSuccess(res.data);
     } catch (err: any) {
       if (axios.isAxiosError(err)) {
@@ -216,17 +235,16 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    block user (block feature have no effect for now)
+  */
   export async function Block(options: {
     username: string,
     onSuccess: () => void;
     onFailure: (err: ErrorResponse) => void;
   }) {
     try {
-      await localService.get("/api/users/relations/block", {
-        params: {
-          username: options.username,
-        }
-      });
+      await localService.get(`/api/users/relations/block/${options.username}`);
       return options.onSuccess();
     } catch (err: any) {
       if (axios.isAxiosError(err)) {
@@ -239,17 +257,16 @@ export namespace Apis {
     return options.onFailure({ message: "something went wrong!" });
   }
 
+  /*
+    unblock user (unblock feature have no effect for now)
+  */
   export async function Unblock(options: {
     username: string,
     onSuccess: () => void,
     onFailure: (err: ErrorResponse) => void;
   }) {
     try {
-      await localService.get("/api/users/relations/unblock", {
-        params: {
-          username: options.username,
-        }
-      });
+      await localService.get(`/api/users/relations/unblock/${options.username}`);
       return options.onSuccess();
     } catch (err: any) {
       if (axios.isAxiosError(err)) {
